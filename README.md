db5eb186759f708ac948a3b58d2a2389aeba9fd2
Juan Ignacio Spadoni <yo_juan_kapo@hotmail.com>
2025-10-30 23:34:10 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Aca Ganaderia
+
---
+++ web/prettier.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  tabWidth: 2,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+};
