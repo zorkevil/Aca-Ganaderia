1d22a489fb138bde74a96e32776184a28b3e87fb
Juan Ignacio Spadoni <yo_juan_kapo@hotmail.com>
2025-12-23 03:22:32 -0300

    Fix alianzas

--- web/src/components/hacienda/HaciendaTabsSection.server.tsx
+++
@@ -1,6 +0,0 @@
-import HaciendaTabsSectionClient from './haciendaTabs/HaciendaTabsSection';
-import TabMAGServer from './tabs/tabMAG.server';
-
-export default async function HaciendaTabsSectionServer() {
-  return <HaciendaTabsSectionClient magTab={<TabMAGServer />} />;
-}
